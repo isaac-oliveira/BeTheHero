665b6d31518009c4d1cc923d7a057efcca543acb
isaac-oliveira <isaacsantos9876@gmail.com>
2020-03-27 20:38:25 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const OngController = require('./controllers/OngController');
+const IncidentsController = require('./controllers/IncidentsController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/session', SessionController.create);
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+module.exports = routes;
